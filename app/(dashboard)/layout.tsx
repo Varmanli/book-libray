7a808a76831b8d3a400c4ba0016fef088c3016b8
varmanli <varmanliamirhosein@gmail.com>
2025-09-18 02:02:47 +0330

    fix:fix nested layout html tag error

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -9,19 +9,11 @@ export default async function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="fa" dir="rtl">
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/gh/rastikerdar/vazir-font@latest/dist/font-face.css"
-        />
-      </head>
-      <body>
-        <Header />
-        <main className="pt-33 md:pt-20">{children}</main>
-        <Toaster position="top-right" />
-        <Footer />
-      </body>
-    </html>
+    <>
+      <Header />
+      <main className="pt-33 md:pt-20">{children}</main>
+      <Toaster position="top-right" />
+      <Footer />
+    </>
   );
 }
